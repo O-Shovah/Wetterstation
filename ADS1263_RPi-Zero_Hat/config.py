fd303830b908a29dab94de1c583b6fcab94170ca
Jens Schmelkus <github@shmelkus.net>
2021-11-10 22:23:57 +0100

    sorting content

--- ADS1263_RPi-Zero_Hat/config.py
+++ ADS1263_RPi-Zero_Hat/config.py
@@ -1,32 +1,3 @@
-# /*****************************************************************************
-# * | File        :	  config.py
-# * | Author      :   Waveshare team
-# * | Function    :   Hardware underlying interface
-# * | Info        :
-# *----------------
-# * | This version:   V1.0
-# * | Date        :   2020-12-12
-# * | Info        :   
-# ******************************************************************************/
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documnetation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to  whom the Software is
-# furished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-
 import spidev
 import RPi.GPIO as GPIO
 import time
@@ -69,4 +40,3 @@ def module_exit():
     SPI.close()
     GPIO.output(RST_PIN, 0)
     GPIO.output(CS_PIN, 0)
-### END OF FILE ###

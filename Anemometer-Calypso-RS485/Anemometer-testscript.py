b29fed31c8e0efc9fed1fb075df1b6cd3150e0ee
Jens Schmelkus <github@shmelkus.net>
2021-11-24 21:35:21 +0100

    Additional Constructors

--- Anemometer-Calypso-RS485/Anemometer-testscript.py
+++ Anemometer-Calypso-RS485/Anemometer-testscript.py
@@ -17,23 +17,24 @@ from influxdb_client.client.write_api import ASYNCHRONOUS
 
 class UserInterface():
 
-    def _init_(self)
+    def _init_(self):
+        self.keyboard_input_str = None
 
-    def keyboard_read_input(KeyboardInputQueue):
+    def keyboard_read_input(self, KeyboardInputQueue):
         #print('Ready for keyboard input: ')
         while (True):
             
-            keyboard_input_str = input()
+            self.keyboard_input_str = input()
 
-            KeyboardInputQueue.put(keyboard_input_str)
+            KeyboardInputQueue.put(self.keyboard_input_str)
 
+    def _init_(self, logLevel):
+        ## for file logging
+        numeric_level = getattr(logging, logLevel.upper(), 10)
+        if not isinstance(numeric_level, int):
+            raise ValueError('Invalid log level: %s' % logLevel)
+        logging.basicConfig(level=numeric_level)
 
-def _init_(self, logLevel):
-    ## for file logging
-    numeric_level = getattr(logging, logLevel.upper(), 10)
-    if not isinstance(numeric_level, int):
-       raise ValueError('Invalid log level: %s' % logLevel)
-    logging.basicConfig(level=numeric_level)
 
 class SerialInterface():
 
@@ -44,12 +45,12 @@ class SerialInterface():
 
         #set the parameters for the serial port
         self.serial_interface_connection = serial.Serial(
-            port='COM4',\
-            baudrate=38400,\
-            parity=serial.PARITY_NONE,\
-            stopbits=serial.STOPBITS_ONE,\
-            bytesize=serial.EIGHTBITS,\
-            timeout=2)
+            port = 'COM4',\
+            baudrate = 38400,\
+            parity = serial.PARITY_NONE,\
+            stopbits = serial.STOPBITS_ONE,\
+            bytesize = serial.EIGHTBITS,\
+            timeout = 2)
 
         print("connected to: " + serial_connection_set_up.portstr)
 
@@ -97,30 +98,38 @@ class SerialInterface():
             time.sleep(0.01)
 
 
-def InfluxDB_upload(SerialInputQueue, winddirection, windspeed, timestamp_received_ns, bucket, org):
+class InfluxDBInterface():
 
-    #The fuction to be threaded for uploading the received values into the influxDB from the Queue
-
-    point = Point("Testrun").tag("Sensor", "Anemometer").field("Winddirection_[deg]", winddirection).field("Windspeed_[m/s]", windspeed).field("Reading_received_timestamp_[ns]", timestamp_received_ns)
-
-    result=write_api.write(bucket, org, point)
-
-    return()
+    def _init_(self):
+        self.write_api = None
 
 
-def influxdb_connection_setup():
+    def influxdb_connection_setup(self):
 
-    #set the basic paramters for the inluxDB point access
+        #set the basic paramters for the inluxDB point access
 
-    token = "vRwyh3JmkRbpEskWtiyuOuSHps3cNV-CYarGcQmqrGJ14ZVrT02FnDIzF8LmvsFXWLRASGZ1kf_v6yvjNqp7lg=="
-    org = "alexandria"
-    bucket = "Wetterstation-Bucket"
+        self.token = "vRwyh3JmkRbpEskWtiyuOuSHps3cNV-CYarGcQmqrGJ14ZVrT02FnDIzF8LmvsFXWLRASGZ1kf_v6yvjNqp7lg=="
+        self.org = "alexandria"
+        self.bucket = "Wetterstation-Bucket"
 
-    client = InfluxDBClient(url="http://192.168.2.3:8086", token=token)
+        client = InfluxDBClient(url = "http://192.168.2.3:8086", token = self.token)
 
-    write_api = client.write_api(write_options=ASYNCHRONOUS)
+        write_api = client.write_api(write_options = ASYNCHRONOUS)
 
-    return([write_api,bucket,org])
+        return()
+
+
+
+    def InfluxDB_upload(self, SerialInputQueue, winddirection, windspeed, timestamp_received_ns):
+
+        #The fuction to be threaded for uploading the received values into the influxDB from the Queue
+
+        point = Point("Testrun").tag("Sensor", "Anemometer").field("Winddirection_[deg]", winddirection).field("Windspeed_[m/s]", windspeed).field("Reading_received_timestamp_[ns]", timestamp_received_ns)
+
+        result = self.write_api.write(self.bucket, self.org, point)
+
+        return(result)
+
 
 def main():
 
